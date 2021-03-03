3ecb255d19fcd2b98d993828d5d69577692c4ebe
Nihal Mohammed <mnihal64@outlook.com>
2021-03-03 22:39:52 +0530

    Add post model and association

--- db/migrate/20210303170328_create_posts.rb
+++ db/migrate/20210303170328_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-
+      t.string :title
+      t.text :body
+      t.integer :user_id, foreign_key: true
+      
       t.timestamps
     end
   end
