5b1b00f12e603c374fa767b433610e34c9fb03ad
Nihal Mohammed <mnihal64@outlook.com>
2021-03-03 22:46:26 +0530

    add comment and related association

--- db/migrate/20210303170911_create_comments.rb
+++ db/migrate/20210303170911_create_comments.rb
@@ -1,7 +1,10 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-
+      t.text :body
+      t.integer :user_id, foreign_key: true
+      t.integer :post_id, foreign_key: true
+      
       t.timestamps
     end
   end
